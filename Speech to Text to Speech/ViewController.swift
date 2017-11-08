23cfc47570da2109216fc9b485433e798e16e705
jochem toolenaar <toolenaar@gmail.com>
2017-11-08 11:00:35 +0100

    Initial Commit

---
+++ Speech to Text to Speech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speech to Text to Speech
+//
+//  Created by jochem toolenaar on 08/11/2017.
+//  Copyright © 2017 jtms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
